3bc6d1a3dcbd4f2f016a67b73817173ac60d6f47
Christos Gkoutzigiannis <chrisgoutzigiannis@gmail.com>
2021-02-23 20:24:55 +0200

    Added getToken method to lexer

--- lex.py
+++ lex.py
@@ -40,7 +40,86 @@ class Lexer:
 
     # Return the next token.
     def getToken(self):
-        pass
+        self.skipComment()
+        self.skipWhitespace()
+
+        token = None
+
+        # Check the first character of this token to see if we can decide what it is.
+        # If it is a multiple character operator (e.g, >=), number, identifier, or keyword then we will process the rest.
+        if self.curChar == '*':
+            token =  Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == ';':
+            token = Token(self.curChar, TokenType.SEMICOLON)
+        elif self.curChar == '.':
+            token = Token(self.curChar, TokenType.DOT)
+        elif self.curChar == '(':
+            token = Token(self.curChar, TokenType.LPARENS)
+        elif self.curChar == ')':
+            token = Token(self.curChar, TokenType.RPARENS)
+        elif self.curChar == ',':
+            token =  Token(self.curChar, TokenType.COMMA)
+        elif self.curChar == '\0':
+            token = Token('', TokenType.EOF)
+        elif self.curChar == '>':
+            token = Token('>', TokenType.GT)
+        elif self.curChar == '<':
+            token = Token('<', TokenType.LT)
+        elif self.curChar == '=':
+            token = Token('=', TokenType.EQ)
+        # Detect if it's a string.
+        elif self.curChar == '\'':
+            # Get characters between quotations.
+            self.nextChar()
+            startPos = self.curPos
+
+            while self.curChar != '\'':
+                # Don't allow special characters in the string. No escape characters, newlines, tabs, or %.
+                if self.curChar == '\r' or self.curChar == '\n' or self.curChar == '\t' or self.curChar == '\\' or self.curChar == '%':
+                    self.abort("Illegal character in string.")
+                self.nextChar()
+
+            tokText = self.source[startPos : self.curPos]  # Get the substring.
+            token = Token(tokText, TokenType.STRING)
+        # Detect if it's a number.
+        elif self.curChar.isdigit():
+            # Leading character is a digit, so this must be a number.
+            # Get all consecutive digits and decimal if there is one.
+            startPos = self.curPos
+            while self.peek().isdigit():
+                self.nextChar()
+            if self.peek() == '.':  # Decimal!
+                self.nextChar()
+
+                # Must have at least one digit after decimal.
+                if not self.peek().isdigit():
+                    # Error!
+                    self.abort("Illegal character in number.")
+                while self.peek().isdigit():
+                    self.nextChar()
+
+            tokText = self.source[startPos : self.curPos + 1]
+            token = Token(tokText, TokenType.NUMBER)
+        elif self.curChar.isalpha():
+            # Leading character is a letter, so this must be an identifier or a keyword.
+            # Get all consecutive alpha numeric characters.
+            startPos = self.curPos
+            while self.peek().isalnum():
+                self.nextChar()
+
+            # Check if the token is in the list of keywords.
+            tokText = self.source[startPos : self.curPos + 1]  # Get the substring.
+            keyword = Token.checkIfKeyword(tokText)
+            if keyword == None:  # Identifier
+                token = Token(tokText, TokenType.IDENT)
+            else:   # Keyword
+                token = Token(tokText, keyword)
+        else:
+            # Unknown token!
+            self.abort("Unknown token: " + self.curChar)
+
+        self.nextChar()
+        return token
 
 
 # Token contains the original text and the type of token.
