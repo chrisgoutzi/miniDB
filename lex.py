1e0c1d8753b563762ede827b7f31f472e6d87243
Christos Gkoutzigiannis <chrisgoutzigiannis@gmail.com>
2021-02-23 20:19:25 +0200

    Added lexer helper functions.

--- lex.py
+++ lex.py
@@ -9,23 +9,33 @@ class Lexer:
 
     # Process the next character.
     def nextChar(self):
-        pass
+        self.curPos += 1
+        if self.curPos >= len(self.source):
+            self.curChar = '\0'    # EOF
+        else:
+            self.curChar = self.source[self.curPos]
 
     # Return the lookahead character.
     def peek(self):
-        pass
+        if self.curPos + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.curPos + 1]
 
     # Invalid token found, print error message and exit.
     def abort(self, message):
-        pass
+        sys.exit("Lexing error. " + message)
 
     # Skip whitespace and newlines.
     def skipWhitespace(self):
-        pass
+        while self.curChar == ' ' or self.curChar == '\t' or self.curChar == '\r' or self.curChar == '\n':
+            self.nextChar()
 
     # Skip comments in the code.
     def skipComment(self):
-        pass
+        if self.curChar == '-':
+            if self.peek() == '-':
+                while self.curChar != '\n':
+                    self.nextChar()
 
     # Return the next token.
     def getToken(self):
