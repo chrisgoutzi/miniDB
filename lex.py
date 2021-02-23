61d1102c4c0739ba46d0a15fd0834618727e2fc4
Christos Gkoutzigiannis <chrisgoutzigiannis@gmail.com>
2021-02-23 20:20:28 +0200

    Added Token class and TokenType enum.

--- lex.py
+++ lex.py
@@ -1,4 +1,5 @@
 import sys
+import enum
 
 class Lexer:
     def __init__(self, input):
@@ -40,3 +41,59 @@ class Lexer:
     # Return the next token.
     def getToken(self):
         pass
+
+
+# Token contains the original text and the type of token.
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText   # The token's actual text. Used for identifiers, strings, and numbers.
+        self.kind = tokenKind   # The TokenType that this token is classified as.
+
+    def __str__(self):
+        return 'Text: ' + self.text + '\n Kind: ' + str(self.kind) + '\n\n'
+
+    @staticmethod
+    def checkIfKeyword(tokenText):
+        for kind in TokenType:
+            if kind.name == tokenText and kind.value >= 100 and kind.value < 200:
+                return kind
+        return None
+
+# TokenType is our enum for all the types of tokens.
+class TokenType(enum.Enum):
+    EOF = -1
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    SEMICOLON = 4
+    COMMA = 5
+    LPARENS = 6
+    RPARENS = 7
+    DOT = 8
+    # Keywords.
+    SELECT = 101
+    FROM = 102
+    WHERE = 103
+    UPDATE = 104
+    SET = 105
+    INSERT = 106
+    VALUES = 107
+    DROP = 108
+    DELETE = 109
+    DATABASE = 110
+    TABLE = 111
+    CREATE = 112
+    INT = 113
+    TEXT = 114
+    INTO = 115
+    INDEX = 116
+    ON = 117
+    PRIMARY = 118
+    KEY = 119
+    INNER = 120
+    JOIN = 121
+    # Operators.
+    EQ = 201
+    ASTERISK = 202
+    LT = 203
+    GT = 204
